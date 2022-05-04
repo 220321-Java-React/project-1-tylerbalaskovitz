b5f70aa35c056677ec2373b89a5c1f655864ec59
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2022-05-03 22:26:13 -0400

    just adding some skeleton classes and new variables for logging into the program

--- src/main/java/com/revature/services/AuthService.java
+++ src/main/java/com/revature/services/AuthService.java
@@ -1,5 +1,31 @@
 package com.revature.services;
 
+import com.revature.models.Employee;
+import com.revature.models.Role;
+
+//You'll need to get this employee data from the DAO and DB
 public class AuthService {
 
+	//typically, you'll want to validate username/password here in the service
+	//by calling some DAO method that gets employees where username = ? and password = ?
+	
+	//hard coded login
+	public Employee login(String username, String password) {
+		if (username.equals("user") && password.equals("password")) {
+			
+			
+			Role role = new Role();
+			
+			
+			return new Employee(10, "Sandy", "Cheeks", role);
+						
+		} else {
+			return null;
+		}
+		
+		
+	}
 }
+	
+	
+	
--- src/main/java/com/revature/services/EmployeeService.java
+++ src/main/java/com/revature/services/EmployeeService.java
@@ -1,5 +1,35 @@
 package com.revature.services;
 
+import java.util.ArrayList;
+
+import com.revature.daos.EmployeeDAO;
+import com.revature.models.Employee;
+
+//The service layer contains additional business logic needed to process rquests/responses
+//It probably won't be doing much here, but it comes into play with more things like login.
 public class EmployeeService {
 
+	//We need an EmployeeDAO class so that we can call our getEmployees() method.
+	EmployeeDAO eDAO = new EmployeeDAO();
+	
+	
+	//this method gets all employees from the DAO (we will call this method from the Handler in the
+	//controller layer).
+	//This method will be pretty simple since we don't really need to process any data - just send it around
+	public ArrayList<Employee> getEmployees(){
+		
+		//get the list of employees from the DAO
+		ArrayList<Employee> employees = eDAO.getEmployees();
+		
+		
+		
+		//return that List of employees.
+		return employees;
+		
+	}
+	
+	
+	
+	
+	
 }
